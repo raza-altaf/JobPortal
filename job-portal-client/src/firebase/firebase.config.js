1d5df17effd07ec4774677567af7e670c2e511bf
raza-altaf <info@mohammedaltaf.in>
2023-12-20 16:08:57 +0530

    initial commit

---
+++ job-portal-client/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFmgGj4Sh6_6tV4uzBThb49MQZIOTzx3Y",
+  authDomain: "jobportal-8ec2c.firebaseapp.com",
+  projectId: "jobportal-8ec2c",
+  storageBucket: "jobportal-8ec2c.appspot.com",
+  messagingSenderId: "1028989192759",
+  appId: "1:1028989192759:web:40f4808557bbf096054db2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
